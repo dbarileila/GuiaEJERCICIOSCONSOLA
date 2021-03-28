90e70b770088ae74667a02c39691f7752d6588bc
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-28 18:52:08 -0300

    ej4

---
+++ CAIGUÍA_EJR4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJR4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Permita el ingreso de frases, finalizando cuando el usuario ingresa “fin”.
+            string igualdad;
+
+            do
+            {
+                Console.Write("Escriba fin para finalizar: ");
+                igualdad = Console.ReadLine();
+
+            } while (igualdad != "fin");
+
+        }
+    }
+}
