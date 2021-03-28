ce35e9ec83e6c5d56c4ca520b237c88eda5f3496
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-28 19:48:15 -0300

    EJERCICIO 5 DEFINITIVO

---
+++ CAIEJR5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIEJR5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lea las teclas presionadas y finalice al presionar Ctrl+F (Ctrl y F al mismo tiempo).
+
+            ConsoleKeyInfo teclas;
+            Console.TreatControlCAsInput = true;
+
+            do
+            {
+                teclas = Console.ReadKey();
+                Console.WriteLine();
+
+                if ((teclas.Key == ConsoleKey.F) &&
+                    ((teclas.Modifiers & ConsoleModifiers.Control) != 0))
+                {
+                    break;
+                }
+
+            } while (true);
+
+
+
+        }
+    }
+}
