53f929868b894aeea4494768ad04c77337523a55
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-28 18:44:59 -0300

    EJR 3, CONSOLA

---
+++ CAIGUÍA_EJ3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJ3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo TECLAS;
+            do
+            {
+                TECLAS = Console.ReadKey(true);
+
+
+            } while (TECLAS.Key != ConsoleKey.X);
+        }
+    }
+}
