094cf0a0a9aec32522db55e563f34c3340f99462
LEILA MAGALI DE BARI <33DE42366985@campus.economicas.uba.ar>
2021-03-28 18:36:56 -0300

    changes

---
+++ CAIGUÍA_EJ2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAIGUÍA_EJ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Solicite el ingreso de un texto cualquiera y lo presente nuevamente al usuario.
+            Console.Write("Ingrese su nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write($"Su nombre es {nombre}");
+
+            Console.ReadKey();
+        }
+    }
+}
